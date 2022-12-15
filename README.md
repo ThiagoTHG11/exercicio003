b721fe2bdbe0c0d53ba3b888f92feedf54d3b783
Thiago <thiago011524@gmail.com>
2022-12-15 15:55:24 -0300

    feito o setup do js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # exercicio003
 exercício da terceira semana 
+[]criar um blog com um header e alguns posts listados
+[]os posts devem ser inseridos via JS
+[]os elementos da pagina devem ser estilizados usando JS
---
+++ script.js
@@ -0,0 +1,39 @@
+const posts = [
+    {
+        title: "lorem ipsum 1",
+        content: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras accumsan, risus in maximus interdum, nunc orci tempus quam, eu auctor elit elit at enim."
+    },
+    {
+        title: "lorem ipsum 2",
+        content: "Mauris vitae purus ac eros molestie auctor. In hac habitasse platea dictumst. Integer commodo velit vitae dui eleifend, hendrerit iaculis libero dapibus. Quisque tempor diam enim, sit amet fringilla libero dictum vel. Phasellus rutrum nec lorem sed scelerisque."
+    },
+    {
+        title: "lorem ipsum 3",
+        content: "Pellentesque cursus metus nec porttitor viverra. Cras bibendum elementum suscipit. Fusce sed erat vitae massa consequat pulvinar."
+    },
+
+];
+
+const section = document.getElementById("posts");
+
+for (let i=0; i<posts.length; i++) {
+    const postData = posts[i];
+
+    const article = document.createElement("article");
+
+    const title = document.createElement("h1");
+
+    const titleText = document.createTextNode(postData.title);
+
+    title.appendChild(titleText);
+
+    article.appendChild(title);
+
+    const content = document.createElement("p");
+    const contentTxt = document.createTextNode(postData.content);
+    content.appendChild(contentTxt);
+    article.appendChild(content);
+
+    section.appendChild(article);
+
+}
